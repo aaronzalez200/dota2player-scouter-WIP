a1425845484a9a355d8ec00a37e8bf6578c60b29
aaronzalez <70921164+aaronzalez@users.noreply.github.com>
2022-07-06 03:01:14 -0400

    ID Input Field

--- src/App.js
+++ src/App.js
@@ -6,28 +6,47 @@ export default function App() {
   // fetching player data
   // user ID below
   // 151097735 129050083 148839639 211537238 297679279
-const steam_id = 129050083
+  let steam_Id = 297679279;
 
-  const [dota2Data, setDota2Data] = React.useState()
+  const [userInput, setUserInput] = React.useState(211537238);
+  const [dota2Data, setDota2Data] = React.useState();
+  // fetch player info
   React.useEffect(function() {
-    fetch(`https://api.opendota.com/api/players/${steam_id}`)
+    fetch(`https://api.opendota.com/api/players/${userInput}`)
         .then(res => res.json())
         .then(data => setDota2Data(data))
-}, []) 
-
+  }, [userInput]) 
+  // User Input
+  const handleChange = event => {
+    setUserInput(event.target.value);
+  };
+  const handleClick = event => {
+    event.preventDefault();
+    setUserInput('New value');
+  };
+  // getting images
 function importAll(r) {
   let images = {};
   r.keys().map((item, index) => {images[item.replace('./', '')] = r(item); });
   return images;
 }
-
 const images = importAll(require.context('./images', false, /\.(png|jpe?g|svg)$/));
-
+  // for loading...
   if(dota2Data === undefined) return <>loading...</>
 
+  
   return (
     <div>
         <Navbar />
+        <input
+          type="text"
+          id="message"
+          name="message"
+          onChange={handleChange}
+          value={userInput}
+        />
+      <h2>Message: {userInput}</h2>
+      <button onClick={handleClick}>Click</button>
         <Profile 
           image={dota2Data.profile.avatarfull} 
           userData={dota2Data.profile.personaname}
