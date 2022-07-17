3a175ff9d829c26efabc51932e31d9c034b21e30
aaronzalez <70921164+aaronzalez@users.noreply.github.com>
2022-07-17 10:05:24 -0400

    Small update, paypal and scrollbars

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -9,7 +9,8 @@ export default function Navbar() {
                 <li><a href="#">Home</a></li>
                 <li><a href="#">About</a></li>
                 <li><a href="#">Sources</a></li>
-                <li><a href="#">Contact</a></li>  
+                <li><a href="#">Contact</a></li>
+                <li><a href="https://www.paypal.me/aaronzalez">Donate</a></li>  
             </ul>
             <h1 className="Title">
                 Dota 2 Player Scouter
